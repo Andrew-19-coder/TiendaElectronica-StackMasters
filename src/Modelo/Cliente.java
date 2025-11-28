669c0e3acdcea9f173a3b4d240f54cc8e2a0d426
Andres_SB <andresthefit17@gmail.com>
2025-11-27 19:41:13 -0600

    Clase Cliente
    Se crea la clase Cliente, con atributos, constructor, getters y setters

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author itsth
+ */
+public class Cliente {
+    private String cedula;
+    private String nombreCompleto;
+    private String telefono;
+    private String correo;
+    private int comprasRealizadas;
+
+    public Cliente(String cedula, String nombreCompleto, String telefono, String correo, int comprasRealizadas) {
+        this.cedula = cedula;
+        this.nombreCompleto = nombreCompleto;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.comprasRealizadas = comprasRealizadas;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getComprasRealizadas() {
+        return comprasRealizadas;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
